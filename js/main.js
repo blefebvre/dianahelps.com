258873746eee4e08aadf7a4f7e3a92c735c6bb7b
Bruce Lefebvre <bruce@brucelefebvre.com>
2019-12-10 22:33:27 -0500

    Enable "back" nav to initial tab

--- js/main.js
+++ js/main.js
@@ -7,20 +7,21 @@
 
   function openTab(tabName) {
     // Is this a valid hash?
-    if (tabs.indexOf(tabName) >= 0) {
-      // Hide current tab
-      document.querySelector(currentTab + "-tab").classList.remove("active");
-      document
-        .querySelector(currentTab + "-content")
-        .classList.add("visuallyhidden");
-
-      // Activate new tab
-      document.querySelector(tabName + "-tab").classList.add("active");
-      document
-        .querySelector(tabName + "-content")
-        .classList.remove("visuallyhidden");
-      currentTab = tabName;
+    if (tabs.indexOf(tabName) < 0) {
+      tabName = tabs[0];
     }
+    // Hide current tab
+    document.querySelector(currentTab + "-tab").classList.remove("active");
+    document
+      .querySelector(currentTab + "-content")
+      .classList.add("visuallyhidden");
+
+    // Activate new tab
+    document.querySelector(tabName + "-tab").classList.add("active");
+    document
+      .querySelector(tabName + "-content")
+      .classList.remove("visuallyhidden");
+    currentTab = tabName;
   }
 
   // Listen for hash changes
@@ -35,5 +36,7 @@
 
   // Activate tab if it's hash is provided initially
   var initialHash = location.hash;
-  openTab(initialHash);
+  if (initialHash) {
+    openTab(initialHash);
+  }
 })();
